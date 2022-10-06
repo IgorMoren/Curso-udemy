db57d02fa264fa6f762246eb5318c6ae4fd3d37d
BOSONIT\igor.moreno <igor.moreno@bosonit.com>
2022-10-06 16:24:10 +0200

    Maps commit

--- 08-lifecycle/src/app/app.module.ts
+++ 08-lifecycle/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { Pagina1Component } from './pages/pagina1/pagina1.component';
 import { MuestraNombreComponent } from './pages/muestra-nombre/muestra-nombre.component';
 import { FormsModule } from '@angular/forms';
 
+
 @NgModule({
   declarations: [AppComponent, Pagina1Component, MuestraNombreComponent],
   imports: [BrowserModule, FormsModule],
